c313536593819e54ed3eda5b592cbd9fdc453329
Nicholas Whyte <nick@nickwhyte.com>
2016-08-02 17:55:58 +1000

    Extract version from tag

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 import os
+import subprocess
 
 PACKAGE_PATH = './twopi_flask_utils'
 
@@ -22,9 +23,15 @@ tests_require = []
 for x in extras.values():
     tests_require.extend(x)
 
+def get_version():
+    v = subprocess.check_output(['git', 'describe', '--dirty', '--always'])
+    if type(v) == str:
+        return v
+    return v.decode('UTF-8')
+
 setup(
     name='twopi-flask-utils',
-    version="1.0.4",
+    version=get_version().strip(),
     package_dir=dict(packages),
     packages=list(map(lambda x: x[0], packages)),
     extras_require=extras,
